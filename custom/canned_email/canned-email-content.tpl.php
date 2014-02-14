2e661dec8962e1aeb8763c0f30995d7c5aa24185
Ross Glover <ross@ross.mayfirst.org>
2014-02-14 03:44:01 -0600

    Adding canned_email module.

---
+++ custom/canned_email/canned-email-content.tpl.php
@@ -0,0 +1,17 @@
+<?php
+// FILE NOT IN USE - COULD BE DELETED
+/**
+ * @file
+ * Example tpl file for theming a single model-specific theme
+ *
+ * Available variables:
+ * - $status: The variable to theme (while only show if you tick status)
+ * 
+ * Helper variables:
+ * - $canned_email: The Model object this status is derived from
+ */
+?>
+
+<div class="canned-email">
+<?php print '<strong>Canned Email Test Data:</strong> ' . $canned_email_content; ?>
+</div>
