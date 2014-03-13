a83e28c5b66549b7867e5c71c467bd2deab5e08a
Ross Glover <ross@ross.mayfirst.org>
2014-03-12 23:13:05 -0600

    Fixing jquery in goals.js

--- custom/asb_issues/js/goal.js
+++ custom/asb_issues/js/goal.js
@@ -1,5 +1,7 @@
 (function($) {
   Drupal.behaviors.asbGoal = {
-    $('.page-node-add-goal #edit-field-scheme-goals-taxonomy').hide();
+    attach: function(context, settings) {
+      $('.page-node-add-goal').find('#edit-field-scheme-goals-taxonomy').hide();
+    }
   }
 })(jQuery);
