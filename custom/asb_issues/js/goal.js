77baa47746db8790fd14194c0b5b206fe3f63d01
Ross Glover <ross@ross.mayfirst.org>
2014-03-13 00:46:54 -0600

    Adding additional jquery to hide workflow on goal edit.

--- custom/asb_issues/js/goal.js
+++ custom/asb_issues/js/goal.js
@@ -3,6 +3,7 @@
     attach: function(context, settings) {
       $('#edit-field-scheme-goals-taxonomy', context).once('asbGoal', function () {
         $(this).hide();
+        $('#edit-workflow').hide();
       });
     }
   }
