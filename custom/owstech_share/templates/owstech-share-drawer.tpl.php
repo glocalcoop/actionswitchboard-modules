756081e87797f83b4619235545538474adf858ac
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-03-17 17:58:49 -0400

    styled share module, made it a comapss project

--- custom/owstech_share/templates/owstech-share-drawer.tpl.php
+++ custom/owstech_share/templates/owstech-share-drawer.tpl.php
@@ -1,13 +1,14 @@
 <section class="owstech-share">
-  <h4 class='actuator'><a title="Share"><?php print $owstech_share_actuator_text; ?></a></h4>
-  <div class="drawer">
-    <ul>
+
+  <h3 class='actuator'><a title="Share"><?php print $owstech_share_actuator_text; ?></a></h3>
+
+    <ul class="drawer">
 
       <!-- @todo, these li's could possibly be turned into a forloop based on the checkbox values from the menu forms -->
       <?php if( $owstech_share_include_facebook ): ?>
       <li>
         <a class="icon facebook" rel="nofollow" title="<?php print $share_cta_fb; ?>" target="_blank" href="<?php print $share_url_fb; ?> ">
-          <?php print $share_cta_fb; ?>
+          <span><?php print $share_cta_fb; ?></span>
         </a>
       </li>
       <?php endif; ?>
@@ -15,7 +16,7 @@
       <?php if( $owstech_share_include_twitter ): ?>
       <li>
         <a class="icon twitter" rel="nofollow" title="<?php print $share_cta_twitter; ?>" target="_blank" href="<?php print $share_url_twitter; ?>">
-          <?php print $share_cta_twitter; ?>
+          <span><?php print $share_cta_twitter; ?></span>
         </a>
       </li>
       <?php endif; ?>
@@ -23,7 +24,7 @@
       <?php if( $owstech_share_include_googleplus ): ?>
       <li>
         <a class="icon google-plus" rel="nofollow" title="<?php print $share_cta_googleplus; ?>" target="_blank" href="<?php print $share_url_googleplus; ?>">
-          <?php print $share_cta_googleplus; ?>
+          <span><?php print $share_cta_googleplus; ?></span>
         </a>
       </li>
       <?php endif;?>
@@ -31,10 +32,11 @@
       <?php if( $owstech_share_include_reddit ): ?>
       <li>
         <a class="icon reddit" rel="nofollow" title="<?php print $share_cta_reddit;?>" target="_blank" href="<?php print $share_url_reddit;?>">
-          <?php print $share_cta_reddit; ?>
+          <span><?php print $share_cta_reddit; ?></span>
         </a>
       </li>
       <?php endif; ?>
+
     </ul>
-  </div>
+
 </section>
