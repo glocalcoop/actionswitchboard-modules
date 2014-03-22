a9a13b2fe631cde33937018aa35b68ba0d801ac2
Ross Glover <ross@ross.mayfirst.org>
2014-03-22 02:50:42 -0600

    Fixing problems with page display after commenting flyspray#90

--- custom/asb_modal/js/after_ajax.js
+++ custom/asb_modal/js/after_ajax.js
@@ -1,8 +1,30 @@
 (function($, Drupal) {
-   // Our function name is prototyped as part of the Drupal.ajax namespace, adding to the commands:
-   Drupal.ajax.prototype.commands.afterAjaxCallbackAsb = function(ajax, response, status) {
-     // Now we close the modal
-     $('a.close')[0].click();
-     $('li.update-tab a')[0].click();
-   };
- }(jQuery, Drupal));
+  // Our function name is prototyped as part of the Drupal.ajax namespace, adding to the commands:
+  Drupal.ajax.prototype.commands.afterAjaxCallbackAsbModal = function(ajax, response, status) {
+    var commentId = Drupal.settings.asb_modal.commentId
+    // Now we close the modal
+    $('a.close')[0].click();
+    // We were having problems with ckeditor wanting to add
+    // to an id that was removed by ajax so we cancel the 
+    // Drupal.freezeheight function to close the window space.
+    $('#freeze-height').remove();
+    // If we're on the update tab we want to reload it to get the
+    // newest content
+    if($('li.update-tab').hasClass('active')) {
+      $('li.update-tab a')[0].click();
+    }
+
+    // Now we take the current comment and place it after it's parent
+    var currentComment = $(commentId).prev('div');
+    var parentComment = $(commentId);
+    parentComment.next('article.comment').append(currentComment);
+    // Then we scroll back to the parent comment
+    $('html, body').animate({
+      scrollTop: $(commentId).offset().top
+    }, 2000);
+  };
+  Drupal.ajax.prototype.commands.afterAjaxCallbackAsb = function(ajax, response, status) {
+    // This unfreezes the height on non-modal comment submissions.
+    $('#freeze-height').remove();
+  };
+}(jQuery, Drupal));
---
+++ custom/asb_scheme/js/active-tab.js
@@ -0,0 +1,13 @@
+(function($, Drupal) {
+   // Our function name is prototyped as part of the Drupal.ajax namespace, adding to the commands:
+   Drupal.ajax.prototype.commands.afterAjaxTabLoad = function(ajax, response, status) {
+     // Remove Active tab from others
+     $('ul.tabs-primary').find('li.active').find('a.active').find('span.element-invisible').remove();
+     $('ul.tabs-primary').find('li.active').find('a.active').removeClass('active');
+     $('ul.tabs-primary').find('li.active').removeClass('active')
+     // Add active class to tab
+     $('li.update-tab').addClass('active');
+     $('li.update-tab').find('a').addClass('active');
+     // Now we close the modal
+   };
+}(jQuery, Drupal));
