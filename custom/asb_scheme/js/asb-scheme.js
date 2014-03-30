baa7c7ea4a1d79c8038b17cf8340de03242e3a38
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-03-30 19:57:36 -0400

    slideshow pager fixes

--- custom/asb_scheme/js/asb-scheme.js
+++ custom/asb_scheme/js/asb-scheme.js
@@ -1,14 +1,18 @@
 (function($) {
+
   Drupal.behaviors.asbScheme = {
     attach: function(context, settings) {
-      $('ul.primary li.ctools-use-ajax a.use-ajax').ajaxComplete(function() { //.click(function() {
+      $('ul.primary li.ctools-use-ajax a.use-ajax').ajaxComplete(function() {
         $('ul.primary a.active').removeClass('active');
         $(this).addClass('active');
       });
     }
   }
-    $(window).bind('load', function() {
-      // we could append this in the HTML just trying to take some of the load off.
+
+  //add necessary markup for the scheme slideshow
+  $(window).bind('load', function() {
+    // we could append this in the HTML just trying to take some of the load off.
+    if( $("#carousel-wrapper .carousel-div").length > 1 ) {
       $("#carousel-wrapper").after('<nav id="scheme-images-nav"><a class="cycle-arrow" id="scheme-images-previous" href="#"><span class="screen-reader-text">Previous</span></a><a id="scheme-images-next" class="cycle-arrow" href="#"><span class="screen-reader-text">Next</span></a></nav>');
       $("#carousel-wrapper").cycle({
         fx:     'fade', 
@@ -19,8 +23,8 @@
         next:   '#scheme-images-next', 
         prev:   '#scheme-images-previous',
         fit: 1
-
-      });
-    });
+      });      
+    }
+  });
 
 })(jQuery);
